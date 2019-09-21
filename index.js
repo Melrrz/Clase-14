896aa6888e2121b12293af5b5029c17e1cf074d7
unknown <mely.rrz@gmail.com>
2019-09-20 21:39:51 -0500

    listo

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 9000
+
+const handleRoute = function (req, res){
+    res.send('Hello World');
+};
+
+app.get ('/', handleRoute);
+
+app.post('/another-route', function (req, res) {
+    console.log(req);
+    res.send(`Hello to another route!`);
+})
+
+app.put('/', function (req,res) { 
+res.send(`this is a put route`);
+})
+
+app.delete('/', function (req, res){
+    res.send(`this is a delete route`);
+})
